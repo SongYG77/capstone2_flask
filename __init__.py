2f537ddaa36b3eabdf56a8a50b57d872ff5e7af2
SongYG <rjawoakstp@naver.com>
2021-08-05 17:52:05 +0900

    add get user reservation data

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
-from model import db, User, Bench, Reck
+from model import db, User, Bench, Reck, Aerobic
 from flask import Flask, render_template, request, redirect , jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-
 
 app = Flask(__name__)
 
@@ -13,25 +11,60 @@ def reserve():
     if request.method == 'GET':
         b = []
         data = Bench.query.all()
-        print(data)
         for i in data:
             a = {"datetime" :i.date+" "+i.time,
                  "name": i.name}
             b.append(a)
-        print(a)
-        print(b)
         return jsonify(b)
     elif request.method == 'POST':
         params = request.get_json()
-        id = params['id']
-        name = params['name']
-        date = params['date']
-        time = params['time']
+        id = params[0]['id']
+        name = params[0]['name']
+        date = params[0]['date']
+        time = params[0]['time']
 
         bench = Bench(id, name, date, time)
         db.session.add(bench)
         db.session.commit()
         return 'OK'
+
+
+@app.route('/bench_reservation_user/<username>',methods=['GET','POST'])
+def benchreserve_user(username):
+    if request.method == 'GET' :
+
+        bench = []
+        benchdata = Bench.query.filter(Bench.name == username).all()
+        for i in benchdata:
+            a = {"datetime" :i.date+" "+i.time,
+                 "name": i.name}
+            bench.append(a)
+        return jsonify(bench)
+
+@app.route('/reck_reservation_user/<username>',methods=['GET','POST'])
+def reckreserve_user(username):
+    if request.method == 'GET' :
+        reck = []
+        reckdata = Reck.query.filter(Reck.name == username).all()
+        for i in reckdata:
+            a = {"datetime": i.date + " " + i.time,
+                 "name": i.name}
+            reck.append(a)
+
+        return jsonify(reck)
+
+@app.route('/aerobic_reservation_user/<username>',methods=['GET','POST'])
+def aerobicreserve_user(username):
+    if request.method == 'GET' :
+        aerobic = []
+        aerobicdata = Aerobic.query.filter(Aerobic.name == username).all()
+        for i in aerobicdata:
+            a = {"datetime": i.date + " " + i.time,
+                 "name": i.name}
+            aerobic.append(a)
+
+        return jsonify(aerobic)
+
 
 if __name__ == "__main__":
     migrate = Migrate()
